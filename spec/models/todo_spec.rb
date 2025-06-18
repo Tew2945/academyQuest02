31b8114c37955965b48ed95f4c4aea6d00e4ff3f
Tew2945 <tew2945@gmail.com>
2025-06-18 08:59:23 +0700

    test: create a unit test

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when creating a new todo' do
+    it 'validates presence of name' do
+      todo = Todo.new(name: nil)
+      expect(todo).not_to be_valid
+      expect(todo.errors[:name]).to include("can't be blank")
+    end
+
+    it 'is valid with a name' do
+      todo = Todo.new(name: "Test Todo")
+      expect(todo).to be_valid
+    end
+  end
 end
