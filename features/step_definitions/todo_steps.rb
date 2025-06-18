e62eaaebb9c3dd8c753c479aee0c07e68290f06c
Tew2945 <tew2945@gmail.com>
2025-06-18 22:18:12 +0700

    test: fix test syntax

--- features/step_definitions/todo_steps.rb
+++ features/step_definitions/todo_steps.rb
@@ -2,12 +2,16 @@ Given("I am on the todos page") do
   visit root_path
 end
 
-When('I fill in {string} with {string}') do |field, value|
-  fill_in field, with: value
+When('I fill in the field with testid {string} with {string}') do |testid, value|
+  find("[data-testid='#{testid}']").set(value)
 end
 
-When('I press {string}') do |button|
-  click_button button
+When('I click the button with testid {string}') do |testid|
+  find("[data-testid='#{testid}']").click
+end
+
+Then('I should see a todo item with text {string}') do |text|
+  expect(page).to have_css('[data-testid="todo-item"]', text: text)
 end
 
 Then('I should see {string}') do |text|
